40726f415f2dbe8be717813cf420d39a867eb124
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2022-10-07 17:03:20 +0900

    feat: Whole PLD generation with assets feat: changed flow of generation:       sh script runs the py script which gen a MD file and then convert it to pdf feat: J/H visualization when generating fix: J/H now can have comma and not a point clean: some cleaning, unused reference and better naming

--- pld-generator/src/generate.py
+++ pld-generator/src/generate.py
@@ -1,6 +1,7 @@
 import github
 import os
 from pld import PLD
+from workload import generate_workload_visualization
 
 GITHUB_ORG_NAME = os.getenv("GITHUB_ORG_NAME") or "noted-eip"
 GITHUB_PROJECT_NAME = os.getenv("GITHUB_PROJECT_NAME") or "Roadmap"
@@ -13,10 +14,11 @@ def generate() -> None:
     issues_object_list = github.get_all_issues_from_project(project_id)
 
     user_stories = [issue.to_user_story() for issue in issues_object_list]
-
     user_stories = sorted(user_stories, key=lambda story: story.repo_name)
 
     pld = PLD(user_stories)
 
-    # NOTE : Placeholder
-    print(pld.to_markdown())
+    with open("pld.md", "w") as f:
+        f.write(pld.to_markdown())
+
+    generate_workload_visualization(user_stories)
--- pld-generator/src/pld.py
+++ pld-generator/src/pld.py
@@ -1,15 +1,69 @@
 from user_story import UserStory
-import md
+import pythonic_md as md
+import os, glob
+
+_HARD_CODED_FILES_PATH = "./hard-coded-pages"
 
 
 class PLD:
+    before_user_stories: dict[str, str]
+    user_stories: list[UserStory]
+    rapports: list[str]
+
     def __init__(self, user_stories=list[UserStory]()):
         self.user_stories: list[UserStory] = user_stories
+        self.before_user_stories = dict[str, str]()
+        self.rapports = list[str]()
+
+    def __read_whole_file(self, filename: str):
+        try:
+            content = ""
+            with open(filename) as file:
+                content = file.read()
+            return content
+        except Exception as e:
+            print(e)
+            return None
+
+    def _generate_rapports(self):
+        for filename in glob.glob(f"{_HARD_CODED_FILES_PATH}/rapports/*.md"):
+            content = self.__read_whole_file(os.path.join(os.getcwd(), filename))
+            self.rapports.append(content)
+
+    def _generate_markdown_before_user_stories(self):
+        self.before_user_stories["Description du document"] = self.__read_whole_file(
+            f"{_HARD_CODED_FILES_PATH}/document-description.md"
+        )
+        self.before_user_stories["Tableau des révisions"] = self.__read_whole_file(
+            f"{_HARD_CODED_FILES_PATH}/revision-table.md"
+        )
+        self.before_user_stories["Présentation du projet"] = self.__read_whole_file(
+            f"{_HARD_CODED_FILES_PATH}/project-presentation.md"
+        )
+        self.before_user_stories["Schéma des livrables"] = self.__read_whole_file(
+            f"{_HARD_CODED_FILES_PATH}/schemas.md"
+        )
 
     def to_markdown(self):
         # TODO : All the other informations
+        self._generate_markdown_before_user_stories()
+        self._generate_rapports()
+
         output = ""
+        for part in [
+            "Description du document",
+            "Tableau des révisions",
+            "Présentation du projet",
+            "Schéma des livrables",
+        ]:
+            output += f"{self.before_user_stories[part]}\n{md.separator()}\n"
+
+        output += f"{md.title('User stories', priority=2)}\n"
         output += f"\n{md.separator()}\n".join(
             [story.to_markdown() for story in self.user_stories]
         )
+
+        rapports_title = "Rapports d'avancement"
+        output += f"\n{md.title(rapports_title, priority=2)}\n"  # chr(39) to workaround the apostrophe in a fstring
+        output += f"\n{md.separator()}\n".join(self.rapports)
         return output
