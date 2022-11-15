5be709d1bb116bf586d0ec2892f3bd5aa64529aa
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2022-11-15 20:49:49 +0900

    feat: all in one script

--- pld-generator/generate_pld.sh
+++ pld-generator/generate_pld.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [[ $# != "2" ]]
+then
+    echo "./generate_pld.sh [height_of_the_pdf] [pdf output]"
+    exit 1
+fi
+
 # generate PLD's markdown
 echo "Generating PLD in markdown..."
 python3 main.py
 # convert markdown to pdf
 echo "Converting markdown to HTML..." 
 gh-md-to-html -o OFFLINE pld.md
-echo "Converting markdown to HTML..." 
+echo "Fixing relative paths..." 
 sed -i "s/\\/images/.\\/images/g" pld.html
 sed -i "s/\\/github-markdown-css/.\\/github-markdown-css/g" pld.html
+wkhtmltopdf --enable-local-file-access --page-width 210mm --page-height "$1"mm --outline-depth 1 pld.html "$2"
 echo "Done"
